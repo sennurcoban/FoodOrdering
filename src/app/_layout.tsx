c3cc78ba55e48c05c22b9c35736ab4611c90f63a
Sennur Coban <ssennurcoban@gmail.com>
2024-02-07 14:04:55 +0300

    type safe components

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,8 +4,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-
-import { useColorScheme } from '@/src/components/useColorScheme';
+import { useColorScheme } from '@/components/useColorScheme.web';
 
 export {
   // Catch any errors thrown by the Layout component.
