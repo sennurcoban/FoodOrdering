a11e3110f7fae2f206dc378a30bef52ab02a8951
Sennur Coban <ssennurcoban@gmail.com>
2024-02-07 14:25:51 +0300

    flatlist

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
-import { View} from "react-native";
+import { View, FlatList } from "react-native";
 import products from "@assets/data/products";
 import ProductListItem from "@components/ProductListItem";
 
 export default function MenuScreen() {
   return (
-    <View>
-    <ProductListItem product={products[1]} />
-    <ProductListItem product={products[3]} />
-    </View>
+    <FlatList
+    data={products}
+    renderItem={({item})=> <ProductListItem product={item}/>}
+    numColumns={2}
+    contentContainerStyle={{gap:10, padding:10}}
+    columnWrapperStyle={{gap:10}}
+    />
   );
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -2,41 +2,48 @@ import Colors from "@/constants/Colors";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Product } from "../types";
 
-export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 type ProductListItemProps = {
-    product: Product;
-}
+  product: Product;
+};
 
-const ProductListItem = ({product}: ProductListItemProps) => {
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image || defaultPizzaImage}} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
   );
-}
+};
 
 export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor:"white",
-    padding:10,
-    borderRadius:20
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   image: {
-    width: '100%',
-    aspectRatio:1
+    width: "100%",
+    aspectRatio: 1,
   },
-  title: { 
-    fontSize: 18, 
+  title: {
+    fontSize: 18,
     fontWeight: "600",
-    marginVertical:10,
+    marginVertical: 10,
   },
   price: {
     color: Colors.light.tint,
-    fontWeight:"bold"
+    fontWeight: "bold",
   },
 });
