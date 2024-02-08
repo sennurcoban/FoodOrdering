ba40fb886424f4933c0789799641817426967bdd
Sennur Coban <ssennurcoban@gmail.com>
2024-02-08 15:42:21 +0300

    changes

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,70 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Stack, useLocalSearchParams } from 'expo-router'
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailScreen = () => {
   const { id } = useLocalSearchParams();
-  return (
-    <View>
-      <Stack.Screen options={{title:"Details: "  + id }} />
-      <Text>ProductDetailScreenvfor id: {id}</Text>
-    </View>
-  )
-}
 
-export default ProductDetailScreen
+  const product = products.find((p) => p.id.toString() === id);
+
+  if (!product) {
+    return <Text>Product not found.</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text>Select Size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <View style={styles.size} key={size}>
+            <Text style={styles.sizeText}>{size}</Text>
+          </View>
+        ))}
+      </View>
+
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  sizes:{
+    flexDirection:'row',
+    justifyContent:"space-around",
+    marginVertical:10
+  },
+  size:{
+    backgroundColor:'gainsboro',
+    width:50,
+    aspectRatio:1,
+    borderRadius:25,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  sizeText:{
+    fontSize:20,
+    fontWeight:'500'
+  }
+});
+export default ProductDetailScreen;
